c0ea5a5957642898bd9a5f14f6211e4fb53e9858
markjasonmedina <markjasonmedina04@gmail.com>
2020-01-17 14:59:54 +0800

    2 and 3 already showing

--- MedinaSevenSegment/Form1.vb
+++ MedinaSevenSegment/Form1.vb
@@ -34,20 +34,33 @@
         End If
 
         If i = 2 Then
-            AA.BackColor = Color.FromArgb(0, 0, 128)
+            AA.BackColor = Color.FromArgb(79, 158, 196)
             BB.BackColor = Color.FromArgb(0, 0, 128)
             CC.BackColor = Color.FromArgb(0, 0, 128)
-            DD.BackColor = Color.FromArgb(0, 0, 128)
-            EE.BackColor = Color.FromArgb(0, 0, 128)
-            FF.BackColor = Color.FromArgb(0, 0, 128)
+            DD.BackColor = Color.FromArgb(79, 158, 196)
+            EE.BackColor = Color.FromArgb(79, 158, 196)
+            FF.BackColor = Color.FromArgb(79, 158, 196)
             GG.BackColor = Color.FromArgb(79, 158, 196)
         End If
 
         If i = 3 Then
-            AA.BackColor = Color.FromArgb(79, 158, 196)
-            DD.BackColor = Color.FromArgb(0, 0, 0)
-            EE.BackColor = Color.FromArgb(0, 0, 0)
-            FF.BackColor = Color.FromArgb(0, 0, 0)
+            AA.BackColor = Color.FromArgb(0, 0, 128)
+            BB.BackColor = Color.FromArgb(0, 0, 128)
+            CC.BackColor = Color.FromArgb(79, 158, 196)
+            DD.BackColor = Color.FromArgb(0, 0, 128)
+            EE.BackColor = Color.FromArgb(0, 0, 128)
+            FF.BackColor = Color.FromArgb(79, 158, 196)
+            GG.BackColor = Color.FromArgb(0, 0, 128)
+        End If
+
+        If i = 4 Then
+            AA.BackColor = Color.FromArgb(0, 0, 128)
+            BB.BackColor = Color.FromArgb(0, 0, 128)
+            CC.BackColor = Color.FromArgb(0, 0, 128)
+            DD.BackColor = Color.FromArgb(0, 0, 128)
+            EE.BackColor = Color.FromArgb(79, 158, 196)
+            FF.BackColor = Color.FromArgb(79, 158, 196)
+            GG.BackColor = Color.FromArgb(0, 0, 128)
         End If
     End Sub
 End Class
