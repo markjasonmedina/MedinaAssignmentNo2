6d00096b4c1cede9fc0d416f33b919796130f5c4
markjasonmedina <markjasonmedina04@gmail.com>
2020-01-17 14:11:05 +0800

    Changed the color of seven segment

--- MedinaSevenSegment/Form1.vb
+++ MedinaSevenSegment/Form1.vb
@@ -24,11 +24,27 @@
         i = i + 1
 
         If i = 1 Then
-            GG.BackColor = Color.FromArgb(0, 0, 0)
+            AA.BackColor = Color.FromArgb(0, 0, 128)
+            BB.BackColor = Color.FromArgb(0, 0, 128)
+            CC.BackColor = Color.FromArgb(0, 0, 128)
+            DD.BackColor = Color.FromArgb(0, 0, 128)
+            EE.BackColor = Color.FromArgb(0, 0, 128)
+            FF.BackColor = Color.FromArgb(0, 0, 128)
+            GG.BackColor = Color.FromArgb(79, 158, 196)
         End If
 
         If i = 2 Then
-            AA.BackColor = Color.FromArgb(0, 0, 0)
+            AA.BackColor = Color.FromArgb(0, 0, 128)
+            BB.BackColor = Color.FromArgb(0, 0, 128)
+            CC.BackColor = Color.FromArgb(0, 0, 128)
+            DD.BackColor = Color.FromArgb(0, 0, 128)
+            EE.BackColor = Color.FromArgb(0, 0, 128)
+            FF.BackColor = Color.FromArgb(0, 0, 128)
+            GG.BackColor = Color.FromArgb(79, 158, 196)
+        End If
+
+        If i = 3 Then
+            AA.BackColor = Color.FromArgb(79, 158, 196)
             DD.BackColor = Color.FromArgb(0, 0, 0)
             EE.BackColor = Color.FromArgb(0, 0, 0)
             FF.BackColor = Color.FromArgb(0, 0, 0)
